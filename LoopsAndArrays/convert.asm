c542685b275f7a0a7cbecbc5cd691a8e2aa9894a
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2018-10-13 19:52:33 -0700

    Finished convert.asm

--- LoopsAndArrays/convert.asm
+++ LoopsAndArrays/convert.asm
@@ -2,24 +2,38 @@ TITLE (convert.asm)
 ; This program will convert from Big Endian to Little Endian
 ; ex. takes 12345678 and reverses the order and stores in 
 ; the label littleEndian: 78563412 
-; CHALLENGE: reverse 12345678 to 87654321
 ; bswap solution accidentally found here: https://stackoverflow.com/questions/46824630/how-do-i-address-the-upper-16-bits-of-a-32-bit-register
 INCLUDE Irvine32.inc 
 .data
 bigEndian BYTE 12h, 34h, 56h, 78h
-bigEnd DWORD 12345678h
 littleEndian DWORD ? 
-backwards DWORD ? 
 .code
-main PROC 
-    mov eax, 00000000h
-    mov al, bigEndian
-    mov ah, bigEndian+1
-    bswap eax
-    mov ah, bigEndian+2
-    mov al, bigEndian+3
-    bswap eax 
-    ; All that is left to do is to assign the littleEndian label
-    ; to hold the contents of eax
+main PROC
+    ; set ecx loop counter to size of bigEndian
+    ; (ecx determines loop iterations)
+    mov ecx, SIZEOF bigEndian
+
+    ; use esi register as index
+    mov esi, 0
+
+    ; the following loop works because bigEndian and
+    ; littleEndian are right next to each other in memory
+
+    ; Loopy McLoopface
+    loopey_loop:
+        mov al, BYTE PTR [littleEndian+(ecx-5)]
+        mov BYTE PTR [littleEndian+esi], al
+        inc esi
+        loop loopey_loop
+
+    ; eax - contains the converted DWORD
+    ; ebx - the original DWORD to convert
+    mov eax, [littleEndian]
+    mov ebx, DWORD PTR [bigEndian]
+
+    ; Now, for a one-liner...
+    mov eax, DWORD PTR [bigEndian]
+    bswap eax ; yeet
+    exit
 main ENDP
 end main
