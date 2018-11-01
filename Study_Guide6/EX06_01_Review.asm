d9428d9569abfb2bb9b5e0b4add4720a60abd454
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2018-10-31 21:58:31 -0700

    Made changes to the first and second exercise

--- Study_Guide6/EX06_01_Review.asm
+++ Study_Guide6/EX06_01_Review.asm
@@ -5,15 +5,17 @@ INCLUDE Irvine32.inc
 
 .code
 main PROC
-    mov ax, FFh
+    mov ax, 0FFFFh
     ; Single instruction to clear high 8 bits
     ; of AX
     mov ah, 0h
     ; test if a number is even or odd, set
     ; the zero flag if even
-    and al, 1h
+    and al, 00000001b
     ; convert from upper to lowercase, or keep
     ; character in lowercase 
-    and al, 11011111b
+    mov al, 42h ; the letter capital B
+    or al, 0100000b ; convert to lowercase
+    exit
 main ENDP
 end main
