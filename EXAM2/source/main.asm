166571a91bfe3350fbd9dac8e81816719362484d
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2018-11-07 22:02:02 -0800

    Finished the kaleidoscope, it looks cool I guess.....

--- EXAM2/source/main.asm
+++ EXAM2/source/main.asm
@@ -6,27 +6,22 @@ TITLE Kaleidoscope (main.asm)
 INCLUDE Irvine32.inc 
 .data
 space BYTE " ",0
+caption BYTE "Are you ready?!",0
+msg BYTE "Press OK if you are ready to collect ", 0dh, 0ah
+    BYTE " your biggest headache... ",0
 .code 
 main PROC
-    call Randomize ; set the starting seed
-    mov al, 0
-    mov ecx, 12 ; loopey loop 12 times
-    L1:
-        mov dl, al
-        call getColor ; returns a number in ebx
-        mov eax, ebx
-        call SetTextColor
-        mov edx, offset space
-        call WriteString 
-        inc al
-        loop L1
-    ;mov eax, 0 * 16 ; replace 0 with color (random int)
-    ;call SetTextColor
-    ;mov edx, offset space
-    ;call WriteString
+    mov ebx, offset caption
+    mov edx, offset msg
+    call Msgbox
 
-    ;mov dh, 12 ; row
-    ;mov dl, 0 ; column
+    mov ecx, 100 ; change the kaleidoscope 100 times
+    hloop:
+        mov eax, 1000 ; delay for one second
+        call Delay
+        call Randomize ; set the starting seed
+        call setColor ; calling subroutine here 
+        loop hloop
 
     exit
 main ENDP
@@ -37,22 +32,83 @@ getColor PROC
 ; A subroutine to get a random color (those defined by Irvine)
 ; and return to the calling function as a decimal 0 - 15
 ; Receives: nothing
-; Returns: ebx <- color, 0 - 15d
+; Returns: eax <- color, 0 - 15d
 ; Requires: Randomize, RandomRange from Irvine32.inc
 ; ------------------------------------------------------------
 
     mov eax, 15d ; colors are from 0 - 15
     call RandomRange
-    mov ebx, eax ; move the random integer into ebx to return
-    imul ebx, ebx, 16
+    ;mov ebx, eax ; move the random integer into ebx to return
+    imul eax, eax, 16
     ret
 
 getColor ENDP
 
 ; ------------------------------------------------------------
-func PROC
+setColor PROC
 ; ------------------------------------------------------------
+;
+; A subroutine to create the kaleidoscope
+; Receives: nothing (the column size is 24 by default)
+; Returns: nothing
+; Requires:                                                  <------------------------------------- change this later
 
-func ENDP
+    mov dh, 0 ; <-- to make sure dh is 0 to save headache
+    mov ecx, 12 ; create an outer loop for rows (12 - 1)
+    incRow: 
+        push ecx
+        mov dl, 0 ; for column increment 
+        mov ecx, 12 ; create an inner loop for columns (12 - 1)
+        incCol: ; use Gotoxy to set colors in all 4 quadrants
+
+            call Gotoxy ; <-- the current cursor position
+
+            ; Set quadrant 2 (in normal xy contexts)
+            call getColor ; returns a color in ebx
+            call SetTextColor ; set the color of the text 
+            push edx ; to save the state of dh above
+            mov edx, offset space
+            call WriteString
+            pop edx
+
+            ; Set quadrant 1
+            push edx
+            neg dl
+            add dl, 23
+            call Gotoxy
+            mov edx, offset space
+            call WriteString
+            pop edx 
+
+            ; Set quadrant 3
+            push edx 
+            neg dh
+            add dh, 23
+            call Gotoxy
+            mov edx, offset space
+            call WriteString
+            pop edx
+            
+            ; Set quadrant 4
+            push edx
+            neg dh
+            add dh, 23
+            neg dl
+            add dl, 23
+            call Gotoxy
+            mov edx, offset space
+            call WriteString
+            pop edx
+
+            inc dl ; shift column position
+            loop incCol
+        
+        pop ecx ; restore ecx value from stack
+        call crlf ; next line bois
+        inc dh ; <-- increment the row bois 
+        loop incRow
+    
+    ret ; subroutine return 
+setColor ENDP
 
 end 
